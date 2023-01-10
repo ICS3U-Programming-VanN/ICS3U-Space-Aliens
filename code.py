78e442b0f9979ff23d0e7178964f1d5020c8ae63
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2023-01-10 16:48:56 +0000

    Added extra placeholder code.

--- code.py
+++ code.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 # Created by: Van Nguyen
 # Created on: Jan, 9 2023
-# Space Aliens Game
+# This program is that "Space Aliens" program on the PyBadge
 
-# Displays "Hello, World!"
-print("Hello, World!")
+
+import ugame
+import stage
+
+
+def game_scene():
+    # This function is the main game game_scene
+    
+    # Displays three blank lines
+    print("\n\n\n")
+    
+    # Displays "Hello, World!" on PyBadge
+    print("Hello, World!")
+    
+    # Repeats the game forever
+    while True:
+        # Placeholder
+        pass
+
+if __name__ == "__main__":
+    game_scene()
