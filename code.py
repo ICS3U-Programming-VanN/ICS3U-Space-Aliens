bbe3de30511e5249c14578b5006c95bb54c34909
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2023-01-11 19:03:51 +0000

    Finished cpt-02 code and added comments.

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Created by: Van Nguyen
 # Created on: Jan, 9 2023
-# This program is that "Space Aliens" program on the PyBadge
+# This program is that "Space Aliens" game on the PyBadge
 
 
 import stage
@@ -11,15 +11,25 @@ import ugame
 def game_scene():
     # This function is the main game game_scene
 
-    # Displays three blank lines
-    print("\n\n\n")
+    # Image bank for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
 
-    # Displays "Hello, World!" on PyBadge
-    print("Hello, World!")
+    # Sets the background to image 0 in the image bank
+    # and sets size to 10x8 tiles of size 16x16
+    background = stage.Grid(image_bank_background, 10, 8)
 
-    # Repeats the game forever
+    # Creates stage for background and sets framerate to 60fps
+    game = stage.Stage(ugame.display, 60)
+
+    # Sets all layers of the sprites and determines the order they show up
+    game.layers = [background]
+
+    # Renders all sprites
+    game.render_block()
+
+    # Repeats forever (game loops)
     while True:
-        # Placeholder
+        # Placeholder keyword
         pass
 
 
