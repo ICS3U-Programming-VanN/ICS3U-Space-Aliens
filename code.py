b4b74a3e471b8eb9e51a7349bb23745bc04d7864
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2023-01-16 18:59:08 +0000

    Finished cpt-06 code, allows for sound to play when user pressed A button.

--- code.py
+++ code.py
@@ -18,18 +18,46 @@ def game_scene():
     # ****Reminder: change the sprites later
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
+    # Keeps track of the state of the buttons
+    a_button = constants.button_states["button_up"]
+    b_button = constants.button_states["button_up"]
+    start_button = constants.button_states["button_up"]
+    select_button = constants.button_states["button_up"]
+
+    # Prepares the sound
+    # Open the "pew.wav" file for reading
+    firing_sound = open("pew.wav", "rb")
+
+    # Accesses the audio module of the ugame library
+    sound = ugame.audio
+
+    # Stops any playing sounds
+    sound.stop()
+
+    # Un-mutes the music/sounds
+    sound.mute(False)
+
     # Sets the background to image 0 in the image bank
     # and sets size to 10x8 tiles of size 16x16
     background = stage.Grid(image_bank_background, 10, 8)
 
     # User's sprite that will continually update
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 100)
+    ship = stage.Sprite(
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
+    )
+
+    lighting_bolt = stage.Sprite(
+        image_bank_sprites,
+        9,
+        int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
+        16,
+    )
 
     # Creates stage for background and sets framerate to 60fps
     game = stage.Stage(ugame.display, 60)
 
     # Sets all layers of the sprites and determines the order they show up
-    game.layers = [ship] + [background]
+    game.layers = [ship] + [lighting_bolt] + [background]
 
     # Renders all sprites
     game.render_block()
@@ -41,11 +69,24 @@ def game_scene():
 
         # IF the user presses the A button
         if keys & ugame.K_X != 0:
-            print("A pressed")
+            # If the A button was pressed, updates button state
+            if a_button == constants.button_states["button_up"]:
+                a_button = constants.button_states["button_just_pressed"]
+            # Checks if the button is still pressed and updates button state
+            elif a_button == constants.button_states["button_just_pressed"]:
+                a_button = constants.button_states["button_still_pressed"]
+        # IF A button is not pressed
+        else:
+            # IF the button has been released and updates button state
+            if a_button == constants.button_states["button_still_pressed"]:
+                a_button = constants.button_states["button_released"]
+            # Updates button state to "button_up"
+            else:
+                a_button = constants.button_states["button_up"]
 
         # IF the user presses the B button
         if keys & ugame.K_O != 0:
-            print("B pressed")
+            pass
 
         # IF the user presses the START button
         if keys & ugame.K_START != 0:
@@ -81,8 +122,13 @@ def game_scene():
         if keys & ugame.K_DOWN != 0:
             pass
 
+        # IF A button is pressed
+        if a_button == constants.button_states["button_just_pressed"]:
+            # Plays the firing sound effect
+            sound.play(firing_sound)
+
         # Renders the sprites
-        game.render_sprites([ship])
+        game.render_sprites([ship] + [lighting_bolt])
 
         # Ensures that the game will run at 60fps by pausing the loops
         game.tick()
--- constants.py
+++ constants.py
@@ -6,11 +6,30 @@
 
 # PyBadge screen size is 160x128 | Sprites are 16x16
 # Declared Constants
+# Screen Size
 SCREEN_X = 160
 SCREEN_Y = 128
+
+# Defines grid size of the screen
 SCREEN_GRID_X = 10
 SCREEN_GRID_Y = 8
+
+# Defines sprite size
 SPRITE_SIZE = 16
+
+# Defines the number of lighting bolts in the game
 TOTAL_NUMBER_OF_LIGHTING_BOLTS = 5
+
+# Defines the frames per second
 FPS = 60
+
+# Defines the sprite movement speed
 SPRITE_MOVEMENT_SPEED = 1
+
+# For button states
+button_states = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released",
+}
